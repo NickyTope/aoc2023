6327877576e6307c9a08bdc6da31d0b81a0042cf
Nicky Tope <nicky.tope@gmail.com>
2023-12-21 09:42:14 +1100

    day1

---
+++ src/day1.ts
@@ -0,0 +1,60 @@
+const testInput = `1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`;
+
+export const part1 = (input: string) => {
+  const lines = input.split("\n");
+  const values: string[] = [];
+  for (const line of lines) {
+    if (!line.length) continue;
+    const numbers: string[] = [];
+    for (const char of line) {
+      if (Number.isInteger(Number(char))) numbers.push(char);
+    }
+    const value = `${numbers[0]}${numbers[numbers.length - 1]}`;
+    values.push(value);
+  }
+  const total = values.reduce((a, b) => a + Number(b), 0);
+  console.log(total);
+}
+
+const part1prod = await Bun.file('src/day1.txt').text();
+
+part1(part1prod);
+
+const testInput2 = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`;
+
+export const part2 = (input: string) => {
+  const lines = input.split("\n");
+  const values: string[] = [];
+  for (let line of lines) {
+    if (!line.length) continue;
+    line = line
+      .replaceAll("one", "o1e")
+      .replaceAll("two", "t2o")
+      .replaceAll("three", "th3ee")
+      .replaceAll("four", "f4ur")
+      .replaceAll("five", "f5ve")
+      .replaceAll("six", "s6x")
+      .replaceAll("seven", "se7en")
+      .replaceAll("eight", "ei8ht")
+      .replaceAll("nine", "n9e");
+    const numbers: string[] = [];
+    for (const char of line) {
+      if (Number.isInteger(Number(char))) numbers.push(char);
+    }
+    const value = `${numbers[0]}${numbers[numbers.length - 1]}`;
+    values.push(value);
+  }
+  const total = values.reduce((a, b) => a + Number(b), 0);
+  console.log(total);
+}
+
+part2(testInput2);
